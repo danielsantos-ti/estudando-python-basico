139db26c18dfb3ba575d0d93dbc9e2f0ca5fc33d
danielsantos-ti <sysdsantos@gmail.com>
2025-10-05 21:00:37 -0300

    Adiciona script para conversão de tipos de dados em Python

---
+++ exercícios-resolvidos/conversao_de_tipos_de_dados.py
@@ -0,0 +1,21 @@
+x = 1
+y = 2.8
+z = 1j
+
+# convertendo de int para float
+a = float(x)
+
+# convertendo de float para int
+b = int(y)    
+
+# convertendo de int para complexo
+c = complex(x) 
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
